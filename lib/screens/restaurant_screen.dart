5970e132ee0dd6f88e3fe2454431ec5681508584
JulianMozi <Jkoripamo@gmail.com>
2022-04-15 02:48:03 +0100

    added restaurant screen navigation

---
+++ lib/screens/restaurant_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  
+  final Restaurant restaurant;
+
+  RestaurantScreen({this.restaurant});
+
+  @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
